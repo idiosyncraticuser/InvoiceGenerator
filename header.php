e0deba4d6694209683db7c637641e16e50aba0a3
Rajneesh Kumawat <rajneeshkumawat2005@gmail.com>
2025-08-06 00:06:15 +0530

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="style.css">
      <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
@@ -53,4 +53,5 @@
 
     <script src="script.js"></script>
 </body>
+
 </html>
